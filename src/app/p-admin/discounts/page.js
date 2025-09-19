eb08098dbb13cee63a1c428f982f76a7a7857b5d
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-19 22:51:27 +0330

    Implementing shopping cart functionality

--- src/app/p-admin/discounts/page.js
+++ src/app/p-admin/discounts/page.js
@@ -3,40 +3,16 @@ import Layout from "@/components/layouts/AdminPanelLayout";
 import styles from "@/components/templates/p-admin/discounts/table.module.css";
 import connectToDB from "@/configs/db";
 import DiscountModel from "@/models/Discount";
+import AddDiscount from "@/components/templates/p-admin/discounts/AddDiscount";
 
 const Discounts = async () => {
   connectToDB();
-  const discounts = await DiscountModel.find({}).lean();
+  const discounts = await DiscountModel.find({}).sort({ _id: -1 }).lean();
 
   return (
     <Layout>
       <main>
-        <section className={styles.discount}>
-          <p>افزودن کد تخفیف جدید</p>
-          <div className={styles.discount_main}>
-            <div>
-              <label>شناسه تخفیف</label>
-              <input placeholder="لطفا شناسه تخفیف را وارد کنید" type="text" />
-            </div>
-            <div>
-              <label>درصد تخفیف</label>
-              <input placeholder="لطفا درصد تخفیف را وارد کنید" type="text" />
-            </div>
-            <div>
-              <label>حداکثر استفاده</label>
-              <input placeholder="حداکثر استفاده از کد تخفیف" type="text" />
-            </div>
-            <div>
-              <label>محصول</label>
-              <select name="" id="">
-                <option value="">قهوه ترک</option>
-                <option value="">قهوه عربیکا</option>
-                <option value="">قهوه اسپرسو</option>
-              </select>
-            </div>
-          </div>
-          <button>افزودن</button>
-        </section>
+        <AddDiscount />
 
         {discounts.length === 0 ? (
           <p className={styles.empty}>کد تخفیفی وجود ندارد</p>

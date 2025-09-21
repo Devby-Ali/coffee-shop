e440c5bf17c05550b91fb9e41bce990be51a75c0
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-21 21:08:32 +0330

    Make Uploader

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,5 +1,8 @@
 import connectToDB from "@/configs/db";
 import ProductModel from "@/models/Product";
+// import fs from "fs";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 export async function POST(req) {
   try {
@@ -33,6 +36,39 @@ export async function POST(req) {
     );
   } catch (err) {
     return Response.json({ message: err }, { status: 500 });
+  }
+}
+
+// Image Uploader
+export async function PUT(req) {
+  const formData = await req.formData();
+  const img = formData.get("img");
+
+  // Validation
+  if (!img) {
+    return Response.json(
+      { message: "Product has not image !!" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const buffer = Buffer.from(await img.arrayBuffer());
+    const filename = Date.now() + img.name;
+
+    await writeFile(
+      path.join(process.cwd(), "public/uploads/" + filename),
+      buffer
+    );
+
+    // ✅
+    return Response.json(
+      { message: "File uploaded successfully :))" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json({ message: err.message }, { status: 500 });
   }
 }
 
